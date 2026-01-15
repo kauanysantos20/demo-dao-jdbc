0188b791f06bf95541f7f968fe7fe156a122a845
kauanysantos20 <kauanysantos.ls04@gmail.com>
2026-01-14 21:42:55 -0300

    delete implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -10,7 +10,7 @@ public interface SellerDao {
 	//reponsavel por inserir no banco de dados o obj que enviar como parametrp
 		void insert(Seller obj);
 		void update(Seller obj);
-		void deleteBuId(Integer id);
+		void deleteById(Integer id);
 	//reponsavel por pegar o ID e consultar no banco de dados um obj com esse ID
 		Seller findById(Integer id);
 		List<Seller> findAll();
